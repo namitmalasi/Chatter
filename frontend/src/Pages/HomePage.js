c43fc350ec89f02470fd3d8f71b0f7c4a3d68e07
Namit Malasi <namitmalasi@gmail.com>
2021-12-25 16:25:07 +0530

    added the login and sign up component

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,7 +1,53 @@
 import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../components/Authentication/Login";
+import SignUp from "../components/Authentication/SignUp";
 
 const HomePage = () => {
-  return <div>homepage</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" color="black" fontFamily="work sans">
+          Chatter
+        </Text>
+      </Box>
+
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px ">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
