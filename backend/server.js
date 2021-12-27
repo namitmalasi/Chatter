210353ba30d5ab9e7b02e19a83e550ee24a85488
Namit Malasi <namitmalasi@gmail.com>
2021-12-27 14:18:55 +0530

    added authentication using jwt and password hashing

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
 const express = require("express");
-const chats = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json()); //to accept json data
 
 app.get("/", (req, res) => {
   res.send("Api is running fine");
 });
 
-app.get("/api/chats", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chats/:id", (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server started at port no ${PORT}`));
